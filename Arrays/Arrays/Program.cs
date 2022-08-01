826eda5991e589cf6784c05f835a78eb86fce0e6
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 13:25:55 +0300

    Arrays
    intership

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Zeynep";
+            students[1] = "Nadir";
+            students[2] = "Elif";
+
+
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+
+    }
+}
